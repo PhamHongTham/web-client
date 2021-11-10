3d179202e79390109b3f4841e546a0fc5c24e343
st-hieucao <hieu.cao@supremetech.vn>
2021-11-10 09:13:52 +0700

    setup folder

---
+++ final-project/src/app/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import '../stylesheets/styles.scss'
+import Home from './pages/home/Home';
+
+function App() {
+  return (
+    <Switch>
+      <Route path='/' exact component={Home}></Route>
+    </Switch>
+  );
+}
+
+export default App;
