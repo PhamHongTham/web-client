60f7482bd1ff23ad7e2755a527603e816f9114e3
st-huunguyen <huu.nguyen@supremetech.vn>
2021-11-10 19:31:59 +0700

    check format

--- final-project/src/app/pages/home/Home.tsx
+++ final-project/src/app/pages/home/Home.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import Header from 'share/component/header/Header'
-import RecommendPost from './partials/RecommendPost/RecommendPost'
+import React from 'react';
+import Header from 'share/component/header/Header';
+import RecommendPost from './partials/RecommendPost/RecommendPost';
 
 const Home = () => {
   return (
     <div>
-      <Header/>
-      <RecommendPost/>
+      <Header />
+      <RecommendPost />
     </div>
-  )
-}
-export default Home
+  );
+};
+export default Home;
--- final-project/src/app/pages/home/partials/RecommendPost/RecommendPost.tsx
+++ final-project/src/app/pages/home/partials/RecommendPost/RecommendPost.tsx
@@ -5,12 +5,12 @@ const RecommendPost = () => {
     <section className="section-recommend-post">
       <div className="container">
         <ul className="row group-item">
-          <Post/>
-          <Post/>
-          <Post/>
-          <Post/>
-          <Post/>
-          <Post/>
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
         </ul>
       </div>
     </section>
