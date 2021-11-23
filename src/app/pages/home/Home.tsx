85344f41cd873d2ba9bbca22c6af3f88aa1c8e3d
st-huunguyen <huu.nguyen@supremetech.vn>
2021-11-23 15:33:44 +0700

    add word break for text

--- src/app/pages/home/Home.tsx
+++ src/app/pages/home/Home.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import Slide from './partials/Slider';
-import Newsfeed from './partials/newsfeed/Newsfeed';
+import Newsfeed from './partials/Newsfeed/Newsfeed';
 
 const Home = () => {
   return (
