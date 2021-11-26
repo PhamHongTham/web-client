31c8083133ad1e78f3cc8b619ac63e43a0f291f7
st-hieucao <hieu.cao@supremetech.vn>
2021-11-26 07:30:40 +0700

    fix slide

--- src/app/pages/home/partials/SlideItem.tsx
+++ src/app/pages/home/partials/SlideItem.tsx
@@ -8,7 +8,7 @@ const SlideItem = ({ post, index }: { post: any; index: number }) => {
       className="post-item"
       style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}
     >
-      <Link to="" className="post-link">
+      <Link to={`/detail/${post.id}`} className="post-link">
         <div className="row">
           <div className="post-image col-6 col-md-12">
             <img src={post.cover} alt=""></img>
@@ -24,7 +24,7 @@ const SlideItem = ({ post, index }: { post: any; index: number }) => {
                 className="author-avatar"
               ></img>
               <div className="author-content">
-                <strong className="author-name">{post.user.displayName}</strong>
+                <p className="author-name">{post.user.displayName}</p>
                 <span className="author-role">CEO and Founder</span>
               </div>
             </div>
